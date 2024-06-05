e09d185c778aa47dd87686caa6b56e2223ad0b43
BasiratHamayoon <basirathumayun@gmail.com>
2024-06-05 05:45:14 -0700

    Programs Uploaded

---
+++ index.js
@@ -0,0 +1,71 @@
+//////////////////////////////////////Program#01/////////////////////////////////////////
+//var badal = true
+//if (badal){
+   // console.log("Barish hoge")
+//}else{
+    //console.log("barish nahi hoge")
+//}
+
+/////////////////////////////////////Program#02//////////////////////////////////////////
+//var cardBalance = +prompt("Please enter the amount of your card")
+//if (cardBalance >= 50){
+  //  console.log("Welcome to Zu Peshawar")
+//}if (cardBalance < 50 ){
+  //  console.log("Please Recharge your card")
+//}
+
+////////////////////////////////////Program#03//////////////////////////////////////////
+//var currentMoney = +prompt("Please Enter how much money you have")
+//var burgerPrice = 350
+//if (currentMoney >= burgerPrice){
+  //  console.log("Enjoy your Burger")
+//}else{
+  //  console.log("You have less amount of Money")
+//}
+
+/////////////////////////////////////Program#04/////////////////////////////////////////
+//var number = +prompt("Please enter a Number")
+//if ( number %2 === 0){
+  //  console.log("You Enter Even number")
+//}else{
+  //  console.log("You enter an Odd number")
+//}
+
+//////////////////////////////////////Program05/////////////////////////////////////////
+//var mango = 50
+//var apple = 50
+//var totalBill = mango + apple
+//console.log(totalBill)
+
+///////////////////////////////////////Program#06//////////////////////////////////////
+//var trafficLightColor = prompt("Please enter the Light color")
+//if(trafficLightColor == "Red"){
+  //  console.log("Please STOP")
+//}else if(trafficLightColor == "Yellow"){
+  //  console.log("Ready")
+//}else if(trafficLightColor == "Green"){
+  //  console.log("Go")
+//}else{
+  //  console.log("Invalid Color")
+//}
+
+/////////////////////////////////////////Program#07/////////////////////////////////////
+//var totalThings = +prompt("Please Enter how many things you buy?")
+//var discount
+//if (totalThings >= 10){
+  //  discount = 20
+//}else if(totalThings >= 5){
+  //  discount = 10
+//}else{
+  //  discount = 0
+//}
+  //  console.log("You have ${discount}% on your shopping")
+
+//////////////////////////////////////////program#08///////////////////////////////////////
+  //var bankBalance = 12000
+  //var withdraw = +prompt("Enter the amount you want to withdraw?")
+  //if (bankBalance >= withdraw){
+    //console.log("Amount successfully withdraw")
+  //}else{
+    //console.log("Itny paisy daly nhi jitny mang rahy ho Bro")
+  //}
